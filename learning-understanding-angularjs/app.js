8abd74cd320c9bab3890ab9bbd87a496c7d0ac5a
Fernando <ffmp777@yahoo.com>
2017-10-08 12:08:50 -0600

    added a form validation service (ngMessages)

--- learning-understanding-angularjs/app.js
+++ learning-understanding-angularjs/app.js
@@ -1,6 +1,9 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages']);
 
-myApp.controller('mainController', function($scope, $log) {
-    console.log($scope);
-    console.log($log);
+myApp.controller('mainController', function($scope, $log, $filter) {
+    $scope.name = 'Bob';
+    $scope.formattedname = $filter('uppercase')($scope.name);
+    
+    $log.info($scope.name);
+    $log.info($scope.formattedname);
 });
