6cb187a25a7e9f19ce4a0984504dba12d0abb245
Fernando <ffmp777@yahoo.com>
2017-10-15 10:24:01 -0600

    directives and two way data binding

--- learning-understanding-angularjs/app.js
+++ learning-understanding-angularjs/app.js
@@ -1,12 +1,13 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
     
-    // Whatever sits on the scope becomes availablel in the view, inside the html attached to the controller.
-    $scope.name = 'Bob';
-
-    $timeout(function() {
-    	$scope.name = 'Belchers';
-    }, 3000);
+    $scope.handle = '';
+    
+    $scope.toLowerCase = function() {
+        
+        return $filter('lowercase')($scope.handle);
+    };
+    
     
 }]);
