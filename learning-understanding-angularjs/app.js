0235a434a65e64a776793a081d354ec8d49a19cf
Fernando <ffmp777@yahoo.com>
2017-10-20 10:16:40 -0600

    angular common directives

--- learning-understanding-angularjs/app.js
+++ learning-understanding-angularjs/app.js
@@ -9,12 +9,13 @@ myApp.controller('mainController', ['$scope', '$filter', function($scope, $filte
         return $filter('lowercase')($scope.handle);
     };
     
+    $scope.characters = 5;
     
-    $scope.$watch('handle', function(newValue, oldValue) {
-        
-        console.log('***********digest cycle***********');
-        console.log('Old value: ' + oldValue);
-        console.log('New value: ' + newValue);
-    });
+    $scope.rules = [
+        { rulename: "Must be 5 characters long" },
+        { rulename: "Must not be use elsewhere" },
+        { rulename: "Must be cool" }
+    ];
     
+//    console.log($scope.rules);
 }]);
