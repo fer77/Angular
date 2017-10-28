1335b202ff7943b614a8bd08db62b8e939457370
Fernando <ffmp777@yahoo.com>
2017-10-28 08:13:16 -0600

    created two separate controllers and views

--- learning-understanding-angularjs/app.js
+++ learning-understanding-angularjs/app.js
@@ -1,37 +1,9 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
+myApp.controller('mainController', ['$scope', function($scope) {
+  $scope.name = 'main controller';
+}]);
 
-    $scope.handle = '';
-
-    $scope.toLowerCase = function() {
-
-        return $filter('lowercase')($scope.handle);
-    };
-
-    $scope.characters = 5;
-
-    // $scope.rules = [
-    //     { rulename: "Must be 5 characters long" },
-    //     { rulename: "Must not be use elsewhere" },
-    //     { rulename: "Must be cool" }
-    // ];
-
-    $http.get('https://swapi.co/api/people/')
-         .success(function(result) {
-          //  console.log(result.results);
-           $scope.people = result.results;
-         })
-         .error(function(data, status) {
-           console.log(data);
-         });
-
-//    console.log($scope.rules);
-
-    $scope.alertClick = function() {
-
-        alert('Clicked!!');
-    }
-
-    $scope.name = 'Bob Belcher';
+myApp.controller('secondController', ['$scope', function($scope) {
+  $scope.name = 'second controller';
 }]);
