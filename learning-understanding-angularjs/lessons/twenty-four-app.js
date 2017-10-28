73a236a7a0dd90e7f3ce0bce4e191acca2589267
Fernando <ffmp777@yahoo.com>
2017-10-28 10:40:31 -0600

    renamed files correctly

--- learning-understanding-angularjs/lessons/twenty-four-app.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('mainController', ['$scope', function($scope) {
-  $scope.name = 'main controller';
-}]);
-
-myApp.controller('secondController', ['$scope', function($scope) {
-  $scope.name = 'second controller';
-}]);
