604eeedc233a33c8c5c7c5a170e5869bc30c4c1d
Fernando <ffmp777@yahoo.com>
2017-10-28 10:41:58 -0600

    created two routes and added them to the page

--- learning-understanding-angularjs/app.js
+++ learning-understanding-angularjs/app.js
@@ -1,12 +1,23 @@
-window.addEventListener('hashchange', function() {
-  console.log(window.location.hash);
-  if(window.location.hash === '#bookmark1') {
-    console.log('Bookmark 1');
-  }
-  if(window.location.hash === '#bookmark2') {
-    console.log('Do an action');
-  }
-  if(window.location.hash === '#bookmark3') {
-    console.log('Do some other action');
-  }
+var myApp = angular.module('myApp', ['ngRoute']);
+
+// $routeProvider is available, because ng-Route is included in the application
+myApp.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'pages/main.html',
+    controller: 'mainController'
+  })
+  .when('/second', {
+    templateUrl: 'pages/second.html',
+    controller: 'secondController'
+  })
 });
+
+myApp.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
+  // $log.info($location.path());
+ $scope.message = 'You are on the main page';
+}]);
+
+myApp.controller('secondController', ['$scope', '$location', '$log', function($scope, $location, $log) {
+ $scope.message = 'You are on the second page';
+}]);
