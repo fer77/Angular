614d8fa8aaa5b5c436a0edbffb07e4b521a15842
Fernando <ffmp777@yahoo.com>
2017-11-11 13:31:35 -0700

    clean up for next lesson

---
+++ learning-understanding-angularjs/lessons/twenty-eight.js
@@ -0,0 +1,28 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+// $routeProvider is available, because ng-Route is included in the application
+myApp.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'pages/main.html',
+    controller: 'mainController'
+  })
+  .when('/second', {
+    templateUrl: 'pages/second.html',
+    controller: 'secondController'
+  })
+  .when('/second/:num', {
+    templateUrl: 'pages/second.html',
+    controller: 'secondController'
+  })
+});
+
+myApp.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
+  // $log.info($location.path());
+ $scope.message = 'You are on the main page';
+}]);
+
+myApp.controller('secondController', ['$scope', '$location', '$log', '$routeParams', function($scope, $location, $log, $routeParams) {
+ $scope.message = 'You are on the second page';
+ $scope.num = $routeParams.num || '';
+}]);
