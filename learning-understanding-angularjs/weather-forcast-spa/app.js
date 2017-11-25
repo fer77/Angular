5bc2850895dc116645bc969b7a0e909d1d5930c3
Fernando <ffmp777@yahoo.com>
2017-11-25 13:07:51 -0700

    added routeParams to forecastController and created two methods to handle temperature and date

--- learning-understanding-angularjs/weather-forcast-spa/app.js
+++ learning-understanding-angularjs/weather-forcast-spa/app.js
@@ -12,11 +12,15 @@ weatherApp.config(function($routeProvider) {
     templateUrl: 'pages/forecast.html',
     controller: 'foreCastController'
   })
+  .when('/forecast/:days', {
+    templateUrl: 'pages/forecast.html',
+    controller: 'foreCastController'
+  })
 });
 
 // SERVICES
 weatherApp.service('cityService', function() {
-	this.city = 'New York, NY';
+	this.city = 'Salt Lake City';
 });
 
 // CONTROLLERS
@@ -28,6 +32,19 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 	});
 }]);
 
-weatherApp.controller('foreCastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('foreCastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+	$scope.apiKey = '6f5a071d58e4bfb4d5f7310070e1ff94'
 	$scope.city = cityService.city;
+	// gets the days from the route if it exists otherwise default is 2 days.
+	$scope.days = $routeParams.days || 2;
+	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast?', {callback: 'JSON_CALLBACK'}, {get: {method: 'JSONP'}});
+	$scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days, appid: $scope.apiKey});
+
+	$scope.convertToFahrenheit = function(degK) {
+		return Math.round((1.8 * (degK - 273)) + 32) + ' F';
+	}
+	$scope.convertToDate = function(dt) {
+		// javascript Date opbject:
+		return new Date(dt * 1000);
+	}
 }]);
