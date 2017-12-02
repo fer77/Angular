84744561259a647a03b30b9168cc798a0407668a
Fernando <ffmp777@yahoo.com>
2017-12-02 11:09:38 -0700

    pulled weather API out of controller into its own service

--- learning-understanding-angularjs/weather-forcast-spa/controllers.js
+++ learning-understanding-angularjs/weather-forcast-spa/controllers.js
@@ -1,19 +1,22 @@
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
 	$scope.city = cityService.city;
 
 	$scope.$watch('city', function() {
 		cityService.city = $scope.city;
 	});
+
+	$scope.submit = function() {
+		$location.path('/forecast');
+	};
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function($scope, $routeParams, cityService, weatherService) {
 	$scope.apiKey = '6f5a071d58e4bfb4d5f7310070e1ff94'
 	$scope.city = cityService.city;
 	// gets the days from the route if it exists otherwise default is 2 days.
 	$scope.days = $routeParams.days || '2';
-	$scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast?', {callback: 'JSON_CALLBACK'}, {get: {method: 'JSONP'}});
-	$scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days, appid: $scope.apiKey});
+	$scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days, $scope.apiKey);
 
 	$scope.convertToFahrenheit = function(degK) {
 		return Math.round((1.8 * (degK - 273)) + 32) + ' F';
--- learning-understanding-angularjs/weather-forcast-spa/services.js
+++ learning-understanding-angularjs/weather-forcast-spa/services.js
@@ -2,3 +2,11 @@
 weatherApp.service('cityService', function() {
 	this.city = 'Salt Lake City';
 });
+
+// Weather API
+weatherApp.service('weatherService', ['$resource', function($resource) {
+	this.GetWeather = function(city, days, apiKey) {
+		var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast?', {callback: 'JSON_CALLBACK'}, {get: {method: 'JSONP'}});
+		return weatherAPI.get({q: city, cnt: days, appid: apiKey});
+	}
+}]);
