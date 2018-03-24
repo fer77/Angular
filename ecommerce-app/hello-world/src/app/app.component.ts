12ca22b0c62040b9f9c03bf5abfd19dce7d8ba57
Fernando <ffmp777@yahoo.com>
2018-03-24 17:34:05 -0600

    ng-if

--- ecommerce-app/hello-world/src/app/app.component.ts
+++ ecommerce-app/hello-world/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FavoriteChnagedEventArgs } from './favorite/favorite.component';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +6,5 @@ import { FavoriteChnagedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tweet = {
-    body: "...",
-    likesCount: 10,
-    isLiked: true
-  }
+  courses = [1, 2];
 }
