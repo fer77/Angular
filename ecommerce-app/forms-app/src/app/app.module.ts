d93acc652e2405401f6944887c64858b3c58f27f
Fernando <ffmp777@yahoo.com>
2018-05-05 22:39:04 -0600

    new: added new 'signup-form' component to project

--- ecommerce-app/forms-app/src/app/app.module.ts
+++ ecommerce-app/forms-app/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupFormComponent,
     ContactFormComponent
   ],
   imports: [
