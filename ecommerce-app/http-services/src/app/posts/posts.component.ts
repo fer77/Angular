87f33f8e2552a486bbdb9332f03a9cfdef96f193
Fernando <ffmp777@yahoo.com>
2018-06-30 14:04:11 -0600

    clean up: extracted methods from Post component to Post service.

--- ecommerce-app/http-services/src/app/posts/posts.component.ts
+++ ecommerce-app/http-services/src/app/posts/posts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'posts',
@@ -8,12 +8,11 @@ import { Http } from '@angular/http';
 })
 export class PostsComponent implements OnInit {
   posts: any[];
-  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http: Http) {}
+  constructor(private service: PostService) {}
   
   ngOnInit() {
-    this.http.get(this.url)
+    this.service.getPosts()
           .subscribe(response => {
             this.posts = response.json();
           });
@@ -22,7 +21,7 @@ export class PostsComponent implements OnInit {
     let post: any = { title: input.value };
     input.value = '';
 
-    this.http.post(this.url, JSON.stringify(post))
+    this.service.createPost(post)
              .subscribe(response => {
                post.id = response.json().id;
                this.posts.splice(0, 0, post);
@@ -30,13 +29,13 @@ export class PostsComponent implements OnInit {
              })
   }
   updatePost(post) {
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({isReady: true}))
+    this.service.updatePost(post)
              .subscribe(response => {
                console.log(response.json());
              });
   }
   deletePost(post) {
-    this.http.delete(this.url + '/' + post.id)
+    this.service.deletePost(post.id)
              .subscribe(response => {
                let index = this.posts.indexOf(post);
                this.posts.splice(index, 1);
