e34feb56f4a6b5863f028dd4ddc1bff7eec410fb
Fernando <ffmp777@yahoo.com>
2018-06-30 13:33:03 -0600

    registered(service): registered Post service

--- ecommerce-app/http-services/src/app/app.module.ts
+++ ecommerce-app/http-services/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { PostService } from './post.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { PostsComponent } from './posts/posts.component';
     FormsModule,
     HttpModule // Sets up all dependencies properly. Choose importing here over providers, if  wanting make full use of everything offered by a module.
   ],
-  providers: [],
+  providers: [
+    PostService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
