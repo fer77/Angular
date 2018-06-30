794fc98633cb9c64b376e175c7fd35824f625721
Fernando <ffmp777@yahoo.com>
2018-06-30 14:42:00 -0600

    fix: added error handling.

--- ecommerce-app/http-services/src/app/posts/posts.component.ts
+++ ecommerce-app/http-services/src/app/posts/posts.component.ts
@@ -15,6 +15,8 @@ export class PostsComponent implements OnInit {
     this.service.getPosts()
           .subscribe(response => {
             this.posts = response.json();
+          }, error => {
+            alert('An unexpected error: ' + error);
           });
   }
   createPost(input: HTMLInputElement) {
@@ -26,19 +28,33 @@ export class PostsComponent implements OnInit {
                post.id = response.json().id;
                this.posts.splice(0, 0, post);
                console.log(response.json());
-             })
+             }, (error: Response) => {
+               if (error.status === 400) {
+                 // this.form.setErrors(error.json());
+               } else {
+                 alert('An unexpected error: ' + error);
+               }
+            });
   }
   updatePost(post) {
     this.service.updatePost(post)
              .subscribe(response => {
                console.log(response.json());
-             });
+             }, error => {
+              alert('An unexpected error: ' + error);
+            });
   }
   deletePost(post) {
-    this.service.deletePost(post.id)
+    this.service.deletePost(post.id) // 345
              .subscribe(response => {
                let index = this.posts.indexOf(post);
                this.posts.splice(index, 1);
-             });
+             }, (error: Response) => {
+               if (error.status === 404) {
+                 alert('Post has been deleted.');
+               } else {
+                 alert('An unexpected error: ' + error);
+               }
+            });
   }
 }
