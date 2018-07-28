8c50b661e4631348dbbb8cda8152c2c70b6aa5de
Fernando <ffmp777@yahoo.com>
2018-07-28 16:03:21 -0600

    imported components and added them to view

--- ecommerce-app/http-services/src/app/app.module.ts
+++ ecommerce-app/http-services/src/app/app.module.ts
@@ -2,7 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
+import { NotFoundComponent } from './not-found/not-found.component'; 
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostService } from './services/post.service';
@@ -14,12 +19,38 @@ import { GithubFollowersService } from './github-followers.service';
   declarations: [
     AppComponent,
     PostsComponent,
-    GithubFollowersComponent
+    GithubFollowersComponent,
+    NavbarComponent,
+    HomeComponent,
+    GithubProfileComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule // Sets up all dependencies properly. Choose importing here over providers, if  wanting make full use of everything offered by a module.
+    HttpModule, // Sets up all dependencies properly. Choose importing here over providers, if  wanting make full use of everything offered by a module.
+    RouterModule.forRoot([
+      { 
+        path: '', 
+        component: HomeComponent 
+      },
+      { 
+        path: 'profile/:username', 
+        component: GithubProfileComponent
+      },
+      { 
+        path: 'followers', 
+        component: GithubFollowersComponent 
+      },
+      { 
+        path: 'posts', 
+        component: PostsComponent 
+      },
+      { 
+        path: '**',
+        component: NotFoundComponent 
+      },
+    ])
   ],
   providers: [
     PostService,
